c8a738635ca1cfbb0b223467752019c6b9ee7b3e
Thiago Miranda Cichoki <thiagocichoki7@gmail.com>
2024-01-09 17:57:04 -0300

    init

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiV1: 'http://localhost:8080',
+    apiV2: 'https://dev-api.portaldoseller.com.br/v2'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiV1: 'https://2a5f-201-49-98-225.ngrok-free.app/manager',
+    apiV2: 'https://dev-api.portaldoseller.com.br/v2'
+};
